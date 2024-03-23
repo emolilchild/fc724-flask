db35f4cf176711f812e78a6ed234eee452a769dd
emolilchild <andreqtqng@gmail.com>
2024-03-23 15:55:04 +0000

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,12 +1,13 @@
-from wtforms import Form, StringField, IntegerField, SelectField, RadioField, TextAreaField
+from wtforms import Form, StringField, IntegerField, SelectField, RadioField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired, Email, Length
 class Questionnaire(Form):
     f_name = StringField(label="f_name", validators=DataRequired())
     l_name = StringField(label="l_name", validators=DataRequired())
     student_number = IntegerField(label="student_number", validators=[DataRequired(),Length(min=6, max=6)])
-    email = StringField("email", validators=[DataRequired(), Email()])
+    email = StringField(label="email", validators=[DataRequired(), Email()])
     qnone = SelectField(label="qnone", validators=DataRequired())
     qntwo = RadioField(label="qntwo", validators=DataRequired())
     qnthree = TextAreaField(label="qnthree", validators=DataRequired())
     qn_four = IntegerField(label="qn_four", validators=DataRequired())
-    qn_five = TextAreaField(label="qnthree", validators=DataRequired())
+    qn_five = TextAreaField(label="qnfive", validators=DataRequired())
+    qn_six = BooleanField(label="qnsix", validators=DataRequired(), choices=[("teacher", "Teacher"), ("friend", "Friends"), ("location", "Location"), ("curriculum", "Curriculum")]
