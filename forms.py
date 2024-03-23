cbfdf774c9b9126cc48b30e3e2dc648367009a07
emolilchild <andreqtqng@gmail.com>
2024-03-23 15:25:30 +0000

    student number

--- forms.py
+++ forms.py
@@ -1,5 +1,7 @@
-from wtforms import Form, StringField
-
+from wtforms import Form, StringField, IntegerField
+from wtforms.validators import DataRequired, Email
 class Questionnaire:
     f_name = StringField(label="f_name", validators=[DataRequired()])
     l_name = StringField(label="l_name", validators=[DataRequired()])
+    student_number = IntegerField(label="student_number", validators=DataRequired())
+    
