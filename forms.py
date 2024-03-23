91ed0cc5d63ed794585205a72bcaf1e1c3fe67c7
emolilchild <andreqtqng@gmail.com>
2024-03-23 15:34:33 +0000

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,7 +1,9 @@
-from wtforms import Form, StringField, IntegerField
+from wtforms import Form, StringField, IntegerField, SelectField
 from wtforms.validators import DataRequired, Email, Length
-class Questionnaire:
+class Questionnaire(Form):
     f_name = StringField(label="f_name", validators=DataRequired())
     l_name = StringField(label="l_name", validators=DataRequired())
     student_number = IntegerField(label="student_number", validators=[DataRequired(),Length(min=6, max=6)])
     email = StringField("email", validators=[DataRequired(), Email()])
+    qnone = SelectField(label="qnone", validators=DataRequired())
+    
