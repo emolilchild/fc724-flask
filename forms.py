4a72d394b365f22e2d1448543e99a7e36424abf1
emolilchild <andreqtqng@gmail.com>
2024-03-23 15:40:47 +0000

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form, StringField, IntegerField, SelectField, RadioField
+from wtforms import Form, StringField, IntegerField, SelectField, RadioField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length
 class Questionnaire(Form):
     f_name = StringField(label="f_name", validators=DataRequired())
@@ -7,4 +7,6 @@ class Questionnaire(Form):
     email = StringField("email", validators=[DataRequired(), Email()])
     qnone = SelectField(label="qnone", validators=DataRequired())
     qntwo = RadioField(label="qntwo", validators=DataRequired())
+    qnthree = TextAreaField(label="qnthree", validators=DataRequired())
+    qn_four = IntegerField(label="qn_four", validators=DataRequired)
     
