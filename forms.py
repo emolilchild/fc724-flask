ef486ebbff2594c3ca00f8b53e9e897501da8591
emolilchild <andreqtqng@gmail.com>
2024-03-24 11:19:51 +0000

    s

--- forms.py
+++ forms.py
@@ -5,14 +5,16 @@ class Questionnaire(Form):
     l_name = StringField(label="Last Name", validators=[DataRequired()])
     student_number = IntegerField(label="Student Number", validators=[DataRequired(),Length(min=6, max=6)])
     email = StringField(label="Email", validators=[DataRequired(), Email()])
-    qnone = SelectField(label="Question One", validators=[DataRequired()], choices=[("FCsciandeng", "FC Science and Engineering"),
+    qnone = SelectField(label="Q1: What course do you take here in GIC?", validators=[DataRequired()], choices=[("FCsciandeng", "FC Science and Engineering"),
                                                                                     ("FCbuisandmanage", "FC Buisness and Management"),
                                                                                     ("PMsciandeng", "PM Science and Engineering"),
                                                                                     ("PMbuisandmanage", "PM Buisness and Management")])
 
 
 
-    qntwo = RadioField(label="qntwo", validators=[DataRequired()], choices=[("a", "A"), ("b", "B"), ("c", "C"), ("d","D"), ("e", "E"), ("f", "F")])
+    qntwo = RadioField(label="What grade did you get for last semester?", validators=[DataRequired()],
+                       choices=[("1", "A"), ("2", "B"), ("3", "C"), ("4","D"), ("5", "E"), ("6", "F")])
+
     qnthree = TextAreaField(label="qnthree", validators=[DataRequired()])
     qn_four = IntegerField(label="qnfour", validators=[DataRequired()])
     qn_five = TextAreaField(label="qnfive", validators=[DataRequired()])
