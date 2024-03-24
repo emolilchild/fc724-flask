643bd337f4335282410a307e14ac580a008b8651
emolilchild <andreqtqng@gmail.com>
2024-03-24 12:27:15 +0000

    .

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import Form, StringField, IntegerField, SelectField, RadioField, TextAreaField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, NumberRange, Length
+from wtforms.validators import DataRequired, Email, Optional, Length
 class Questionnaire(FlaskForm):
     f_name = StringField(label="First Name", validators=[DataRequired()])
     l_name = StringField(label="Last Name", validators=[DataRequired()])
@@ -16,12 +16,12 @@ class Questionnaire(FlaskForm):
     qn_three = TextAreaField(label="Q3: How do you feel about the grade you have obtained?", validators=[DataRequired()])
 
     qn_four = IntegerField(label="How satisfied are you with your academic experience here in GIC?",
-                           validators=[DataRequired(), NumberRange(min=1, max=10)])
+                           validators=[DataRequired()])
     qn_five = TextAreaField(label="Q5: Is there anything you would change about the curriculum?", validators=[DataRequired()])
-    teachers = BooleanField(label="Teachers", validators=[DataRequired()])
-    friends = BooleanField(label="Friends", validators=[DataRequired()])
-    location = BooleanField(label="Location", validators=[DataRequired()])
-    curr = BooleanField(label="Curriculum", validators=[DataRequired()])
+    teachers = BooleanField(label="Teachers", validators=[Optional()])
+    friends = BooleanField(label="Friends", validators=[Optional()])
+    location = BooleanField(label="Location", validators=[Optional()])
+    curr = BooleanField(label="Curriculum", validators=[Optional()])
     qn_seven = TextAreaField(label="Q7: Is there any other feedback you would like to add?", validators=[DataRequired()])
     qn_eight = RadioField(label=" Q8: How would you rate this questionnaire?", validators=[DataRequired()],
                           choices=[("1", "Very Good"), ("2", "Good"), ("3", "Neutral"), ("4", "Bad"), ("5", "Very Bad")])
