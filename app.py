85277632b95addc2a126e4397d1aab3b1188d0e6
emolilchild <andreqtqng@gmail.com>
2024-03-24 14:48:10 +0000

    fina

--- app.py
+++ app.py
@@ -17,6 +17,17 @@ def info():
 def data():
     form = Questionnaire()
     if request.method == 'POST':
+        with open('collected_data.txt', 'a') as f:
+            f.write(f"First name: {form.f_name.data}\n")
+            f.write(f"Last name: {form.l_name.data}\n")
+            f.write(f"Student number: {form.student_number.data}\n")
+            f.write(f"Email: {form.email.data}\n")
+            f.write(f"Question 1: {form.qn_one.data}\n")
+            f.write(f"Question 2: {form.qn_two.data}\n")
+            f.write(f"Question 3: {form.qn_three.data}\n")
+            f.write(f"Question 5: {form.qn_five.data}\n")
+            f.write(f"Question 7: {form.qn_seven.data}\n")
+            f.write(f"Question 8: {form.qn_eight.data}\n")
         return "Submission completed"
     return render_template("datacollection.html", form=form)
 
